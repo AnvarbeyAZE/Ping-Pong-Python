eac3b03ec889593639b330b34bd9e66e91d505a1
Anvarbey Muminov <anvarbey.creative@gmail.com>
2019-02-08 20:05:50 +0500

    collisions are added

--- pong.py
+++ pong.py
@@ -88,7 +88,11 @@ while True:
       ball.goto(0,0)
       ball.dx *= -1
 
-    #Paddle and ball collisions
-    #if ball.xcor() > 340 and (ball.ycor()<)
+     # Paddle and ball collisions
+    if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.dx *= -1
+       # os.system("afplay bounce.wav&")
 
-
+    elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.dx *= -1
+       # os.system("afplay bounce.wav&")
